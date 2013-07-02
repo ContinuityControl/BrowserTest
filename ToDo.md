6f69231323a45a36f6e3c0eb8dca970e1d0b4a82
Mitchell Kiss <jmkiss@kti.cc>
2013-07-02 16:55:46 -0400

    initial rails commit

---
+++ ToDo.md
@@ -0,0 +1,29 @@
+The list- 
+* [-] IP( have to come back and redo it)- sorta done 
+	1. server side
+	2. jquery call to someleses server 
+	3. funky java script 
+
+* [x] User Agent
+* [ ] OS- 
+* [ ] Platform
+* [ ] Browser + Version
+* [ ] Local Date/Time
+* [x] Cookies Enabled
+* [ ] JavaScript Enabled 
+* [ ] Flash 
+* [ ] Screen Dimensions
+* [ ] Browser Dimensions
+a
+
+jquery - in 
+modinizer -in 
+
+
+commit gitignore
+commit changes to bootstrap 
+commit rails init 
+move bootstrap into view 
+make controler for view 
+switch to using gems (bootstrap and modernizer)
+get ride of public rails index page 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrowserTest::Application.config.secret_key_base = 'c78d9691bf91f830cb8099c6b12a85e716d1f1ab8a305d963bf4a4af050cddecfa0585e4cf78154719328eb77853e5482ce9ee20fd82ff1e25ad5372fc7e90e5'
