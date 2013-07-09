3529592efa4e718de46c15c18f990ca7d21a0db7
Mitchell Kiss <jmkiss@kti.cc>
2013-07-09 13:51:08 -0400

    added a test to load javascript from remote server

--- app/assets/javascripts/browser_test.js
+++ app/assets/javascripts/browser_test.js
@@ -1,28 +1,34 @@
 $(function(){
-  $("#userAgent").text(navigator.userAgent);
-  replaceStyle("#userAgent");
-  $("#windowSize").text( window.session.device.viewport.width + " x "+ window.session.device.viewport.height);
-  replaceStyle("#windowSize");
-  $("#screenSize").text(window.session.device.screen.width + " x " + window.session.device.screen.height);
-  replaceStyle("#screenSize");
-  $("#os").text(window.session.browser.os);
-  replaceStyle("#os");
-  $("#browser").text(window.session.browser.browser + " Version: " + window.session.browser.version);
-  replaceStyle("#browser");
-  $("#flashEnabled").text(window.session.plugins.flash);
-  replaceStyle("#flashEnabled");
-  $("#date").text(Date());
-  replaceStyle("#date");
-  $('#image1')
-	.load('https://d2b75q7u5jtkag.cloudfront.net/assets/cc_logos/CC_logo_white-230aa004e30cf403e251042f65d3a798.png',function(){
-		$('#picture').text('Loaded');	
+	$("#userAgent").text(navigator.userAgent);
+	replaceStyle("#userAgent");
+	$("#windowSize").text( window.session.device.viewport.width + " x "+ window.session.device.viewport.height);
+	replaceStyle("#windowSize");
+	$("#screenSize").text(window.session.device.screen.width + " x " + window.session.device.screen.height);
+	replaceStyle("#screenSize");
+	$("#os").text(window.session.browser.os);
+	replaceStyle("#os");
+	$("#browser").text(window.session.browser.browser + " Version: " + window.session.browser.version);
+	replaceStyle("#browser");
+	$("#flashEnabled").text(window.session.plugins.flash);
+	replaceStyle("#flashEnabled");
+	$("#date").text(Date());
+	replaceStyle("#date");
+
+	if(control.datepicker_opts != undefined){
+		replaceStyle("#script");
+		$("#script").text("loaded");
+	}
+	else{
+		$( "#script" ).text( "Script failed to load" );
+	}
+
+	if($("#image1").width() == 181 && $("#image1").height() == 74){
+		$("#picture").text("loaded");
 		replaceStyle("#picture");
-	})
-	.error(function(){
-		$('#picture').text('Unable to load');
-	});
-
-
+	}
+	else{
+		$("#picture").text("not loaded");
+	}
 });
 function replaceStyle(domID)
 {
