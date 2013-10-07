3d0a8e137ce4f5c0f6395e04b45b455ea26d5349
Benjamin Oakes <boakes@continuity.net>
2013-10-07 12:20:48 -0400

    (m) formatting

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,6 @@
 class Mailer < ActionMailer::Base
- def send_data(user_datum)
- 	@user_datum = user_datum
- 	mail(to: ENV["to_email_address"] ,from: @user_datum.email_address, subject: "Sending my Data to continuity")
- end
+  def send_data(user_datum)
+    @user_datum = user_datum
+    mail(to: ENV["to_email_address"], from: @user_datum.email_address, subject: "Sending my Data to continuity")
+  end
 end
