71cdae25e37ce272cf8a7b0133b24563f49bd781
Joel Nimety <jnimety@continuity.net>
2014-12-31 11:33:44 -0500

    add X-UA-Compatible header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :x_ua_compatible
+  def x_ua_compatible
+    response.headers['X-UA-Compatible'] = 'IE=edge'
+  end
 end
