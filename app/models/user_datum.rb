ad00ca6f65c5f2c98367871e738c14c22e26e9b4
Joel Nimety <jnimety@continuity.net>
2017-06-14 20:05:59 +0000

    models now inherit from ApplicationRecord

--- app/models/user_datum.rb
+++ app/models/user_datum.rb
@@ -1,4 +1,4 @@
-class UserDatum < ActiveRecord::Base
+class UserDatum < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email_address, presence: true,
             format: {with: VALID_EMAIL_REGEX})
